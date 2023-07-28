92b62e851c1a4c44ba7fc770c1fd9b8d9f25ba5a
WooLim Lee <108933788+woorym@users.noreply.github.com>
2023-07-28 23:50:17 +0900

    [Silver II] Title: 랜선 자르기, Time: 128 ms, Memory: 115136 KB -BaekjoonHub

---
+++ 백준/Silver/1654. 랜선 자르기/랜선 자르기.py
@@ -0,0 +1,24 @@
+import sys
+
+input = sys.stdin.readline
+
+k, n = map(int, input().split())
+arr = [
+    int(input()) 
+    for _ in range(k)
+    ]
+
+start, end = 1, max(arr) 
+
+while start <= end:
+    mid = (start + end) // 2 
+    cnt = 0 
+    for i in arr:
+        cnt += i // mid
+        
+    if cnt >= n:
+        start = mid + 1
+    else:
+        end = mid - 1
+        
+print(end)
