934e3ad410cc88066bcf7e98c9e22062237653d3
WooLim Lee <108933788+woorym@users.noreply.github.com>
2024-01-14 11:07:13 +0900

    [Silver I] Title: 정수 삼각형, Time: 136 ms, Memory: 111444 KB -BaekjoonHub

---
+++ 백준/Silver/1932. 정수 삼각형/정수 삼각형.py
@@ -0,0 +1,24 @@
+#백준 1932번
+import sys
+input = sys.stdin.readline
+
+n = int(input())
+dp = []
+
+for _ in range(n):
+    dp.append(list(map(int, input().split())))
+
+# print(dp)
+for i in range(1, n):
+    for j in range(0, i + 1):
+        if j == 0:
+            dp[i][0] += dp[i - 1][0] 
+
+        elif i == j:
+            dp[i][j] += dp[i - 1][j - 1]
+        
+        else:
+            dp[i][j] += max(dp[i - 1][j - 1], dp[i - 1][j])
+
+# print(dp)
+print(max(dp[n - 1]))
